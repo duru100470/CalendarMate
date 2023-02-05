087e34242d05a7f309a741e8fb41d57c31fdd828
duru100470 <zenpia@naver.com>
2023-02-06 05:39:51 +0900

    Init project

---
+++ ClientApp/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Home from "./Home/Home.svelte";
+	import Test from "./Test/Test.svelte";
+	import NotFound from "./Error/404.svelte";
+	import Router from "svelte-spa-router";
+	const routes = {
+		'/': Home,
+		'/Test': Test,
+		'/*': NotFound
+	};
+</script>
+
+<main>
+	<Router {routes} />
+</main>
